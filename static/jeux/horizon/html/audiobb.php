227e337130b7325e5a7a721202fb7de0e44018e3
Pakal72 <pascal@choquet.org>
2025-07-22 17:31:36 +0200

    out

---
+++ static/jeux/horizon/html/audiobb.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Musique d'Ambiance</title>
+</head>
+<body onload="document.getElementById('background-music').play();">
+    <audio id="background-music" autoplay loop>
+        <source src="jeux/horizon/musique/ambiance.mp3" type="audio/mpeg">
+        Votre navigateur ne supporte pas l'élément audio.
+    </audio>
+</body>
+</html>
