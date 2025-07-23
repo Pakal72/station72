b27b0b9155f5f6ec6bf8990d22a1161207d220d2
Pakal72 <pascal@choquet.org>
2025-07-23 10:31:29 +0200

    ip2

--- main.py
+++ main.py
@@ -489,4 +489,4 @@ def duplicate_transition(transition_id: int):
 
 if __name__ == "__main__":
     
-    uvicorn.run("main:app", host="0.0.0.1", port=8000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
