9bc87c03fd28468ce3f1794f61058cbff848aa67
Pakal72 <pascal@choquet.org>
2025-07-23 10:20:18 +0200

    ecoute tout ip

--- jouer.py
+++ jouer.py
@@ -161,4 +161,4 @@ def jouer_page(request: Request, jeu_id: int, page_id: int, saisie: str = Form("
 
 
 if __name__ == "__main__":
-    uvicorn.run("jouer:app", host="127.0.0.1", port=8001, reload=True)
+    uvicorn.run("jouer:app", host="0.0.0.1", port=8001, reload=True)
