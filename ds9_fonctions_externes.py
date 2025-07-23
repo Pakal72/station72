0f2599c75a1d25594c5f16d62ff49a07640c7719
Pakal72 <pascal@choquet.org>
2025-07-23 15:00:37 +0200

    go

---
+++ ds9_fonctions_externes.py
@@ -0,0 +1,12 @@
+def ds9_cafe() -> str:
+    reponse = (
+        "Cette réponse simule simplement le fait que j'ai appuyé sur le bouton de la cafetière !"
+        "Mais en fait je pourrais mettre un appel HA, un call d'un .exe, ...."
+    )
+    return(reponse)
+
+def cf_ca() -> str:
+    reponse = (
+        "Je lance la fonction cf_ca"
+    )
+    return(reponse)
